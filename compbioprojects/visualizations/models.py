6160f08f12278aa1229c8c39815011c170aeffc3
Michael <mnath10@stuy.edu>
2020-08-16 19:53:58 -0400

    created database schema and included sqlite3 file

--- compbioprojects/visualizations/models.py
+++ compbioprojects/visualizations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DemoDNA(models.Model):
+    # string is the dna string itself. The string can be as long as possible.
+    string = models.TextField()
+    # module represents the module that this dna is from.
+    module = models.TextField()
+
+    def __repr__(self):
+        return "Module: {} DNA: {}".format(module, string)
+
+    def __str__(self):
+        print("Module: {} DNA: {}".format(module, string))
