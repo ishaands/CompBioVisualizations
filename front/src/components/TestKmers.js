60cf02028ec28948b178138ce439eb93144c0227
Michael <mnath10@stuy.edu>
2020-10-18 13:55:32 -0400

    shared_kmers works with dummy data

--- front/src/components/TestKmers.js
+++ front/src/components/TestKmers.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { motion } from "framer-motion";
-// state vars representing the future substring arrays
 
 const TestKmers = (props) => {
+	// this is for the first stage of the algorithm, where substrings get loaded in from backend
 	const [substringsOne, setSubstringsOne] = React.useState([]);
 	const [substringsTwo, setSubstringsTwo] = React.useState([]);
+
+	// state variable representing the current algorithm
 	const [index, setIndex] = React.useState(0);
+
 	// state vars representing the two input strings
 	const [dnaOne, setDnaOne] = React.useState("ATTAG");
-	const [dnaTwo, setDnaTwo] = React.useState("ATTAG");
-	// create a list of button elems that represent the "steps" of the algorithm
-	// const algoButtons = []
+	const [dnaTwo, setDnaTwo] = React.useState("ATTGA");
+
+	const [matchedSubstrings, setMatchedSubstrings] = React.useState([]);
+
+	const [longestSubString, setLongestSubstring] = React.useState("");
 	// function to be fired once user clicks on the startAlgo button
 	const startAlgo = () => {
 		// dummy substrings that mimic fetching from the backend
 		setSubstringsOne(["ATT", "TTA", "TAG"]);
-		setSubstringsTwo(["ATT", "TTA", "TAG"]);
+		setSubstringsTwo(["ATT", "TTG", "TGA"]);
 		incrementIndex();
 	};
 
@@ -24,13 +29,28 @@ const TestKmers = (props) => {
 	const matchSubs = () => {
 		// get match data from backend
 		incrementIndex();
+		setSubstringsOne([]);
+		setSubstringsTwo([]);
+		setMatchedSubstrings(["ATT"]);
 	};
+
+	// function to be fired once matching substrings have been displayed and user wants to get the longest one
+	const longestSub = () => {
+		// get longest data from backend
+		incrementIndex();
+		setMatchedSubstrings([]);
+		setLongestSubstring("ATT");
+	};
+	// create a list of buttons that will initiate a given algorithm stage
 	const algoButtons = [
 		<button id="startAlgo" onClick={startAlgo}>
 			Start Algorithm
 		</button>,
 		<button id="matchSubs" onClick={matchSubs}>
 			Match Substrings
+		</button>,
+		<button id="longestSub" onClick={longestSub}>
+			Get Longest Substring
 		</button>,
 		<button style={{ visbility: "none" }}></button>,
 	];
@@ -46,6 +66,29 @@ const TestKmers = (props) => {
 			<h1>DNA One: {dnaOne}</h1>
 			<h1>DNA Two: {dnaTwo}</h1>
 			<div>
+				{longestSubString && (
+					<motion.h3
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1, color: "#50c878" }}
+						transition={{ duration: 1, delay: 0.5 }}
+						style={{ display: "inline" }}
+					>
+						{longestSubString}
+					</motion.h3>
+				)}
+				{matchedSubstrings.map((val, key) => {
+					return (
+						<motion.h3
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1, color: "#50c878" }}
+							transition={{ duration: 1, delay: 0.5 * key }}
+							style={{ display: "inline" }}
+							key={key}
+						>
+							{val}{" "}
+						</motion.h3>
+					);
+				})}
 				{substringsOne.map((val, key) => {
 					return (
 						<motion.h3
